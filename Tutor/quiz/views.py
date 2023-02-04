d118dc9bafb128f6e8a1c3cc3e78c058885085df
Burhanuddin Kader <burhankader5252@gmail.com>
2023-02-04 15:14:18 +0530

    updated

--- Tutor/quiz/views.py
+++ Tutor/quiz/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 from django.conf import settings
 from datetime import date, timedelta
 from django.db.models import Q
-# from django.core.mail import send_mail
 from student import models as SMODEL
 from student import forms as SFORM
 from django.contrib.auth.models import User
